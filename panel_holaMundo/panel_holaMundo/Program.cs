afc8c92268d9a0c725af63d785f7377575210f23
crivaly <crivaly@icloud.com>
2015-01-21 09:30:48 +0100

    actualización

---
+++ panel_holaMundo/panel_holaMundo/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using Gtk;
+
+namespace panel_holaMundo
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
